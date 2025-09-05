dec620e72dd8fef5532b0e43be73f80b81beb9d1
zerd90 <569440857@qq.com>
2025-09-05 18:01:32 +0800

    miss file

---
+++ src/logger.cpp
@@ -0,0 +1,109 @@
+
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <string>
+#include<algorithm>
+
+#include "logger.h"
+
+using std::string;
+
+namespace Log
+{
+	LOG_LEVEL log_level = LOG_LEVEL_ERR;
+	void set_log_level(LOG_LEVEL level)
+	{
+		if(level < LOG_LEVEL_ERR || level  >= LOG_LEVEL_BUTT)
+			return;
+		log_level = level;
+	}
+	LOG_LEVEL get_log_level()
+	{
+		return log_level;
+	}
+}
+
+std::wstring stringToWstring(const std::string &orig)
+{
+    if (orig.empty())
+    {
+        return std::wstring();
+    }
+    std::wstring res(orig.length() + 1, 0);
+    mbstowcs(&res[0], orig.c_str(), orig.length());
+
+    return res;
+}
+
+std::string wstringToString(const std::wstring &wstr)
+{
+    if (wstr.empty())
+    {
+        return std::string();
+    }
+    char *result;
+    int   textlen;
+    textlen = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, NULL, 0, NULL, NULL);
+    result  = (char *)malloc((textlen + 1) * sizeof(char));
+    memset(result, 0, sizeof(char) * (textlen + 1));
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, result, textlen, NULL, NULL);
+    string res(result);
+    free(result);
+    return res;
+}
+
+std::string getBaseName(std::string &path)
+{
+    size_t lastSlash = path.rfind('\\');
+    if (path.npos == lastSlash)
+        lastSlash = path.rfind('/');
+    if (path.npos == lastSlash)
+        lastSlash = 0;
+    else
+        lastSlash++;
+
+    return path.substr(lastSlash, path.rfind('.') - lastSlash);
+}
+
+std::string getBaseName(std::string &&path)
+{
+	size_t lastSlash = path.rfind('\\');
+    if (path.npos == lastSlash)
+        lastSlash = path.rfind('/');
+    if (path.npos == lastSlash)
+        lastSlash = 0;
+    else
+        lastSlash++;
+
+    return path.substr(lastSlash, path.rfind('.') - lastSlash);
+}
+
+void str_insert(char *str, uint64_t max_size, char c, int pos)
+{
+	uint64_t str_size = strlen(str);
+	if (str_size + 2 > max_size)
+	{
+		Z_ERR("size too small %d, %d\n", str_size, max_size);
+		return;
+	}
+	char *dst = NULL;
+
+	dst = (char *)calloc(str_size + 2, 1);
+	if (!dst)
+	{
+		Z_ERR("alloc fail\n");
+		return;
+	}
+
+	if (pos > 0)
+		memcpy(dst, str, pos);
+	dst[pos] = c;
+	memcpy(dst + pos + 1, str + pos, str_size - pos + 1);
+
+	memcpy(str, dst, str_size + 2);
+
+	free(dst);
+}
