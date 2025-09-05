ae78975316bd2b6c6c5571515e580e0b3aa031e5
zerd90 <569440857@qq.com>
2025-09-05 18:44:59 +0800

    Fix cmake error

--- common.cmake
+++ common.cmake
@@ -3,29 +3,29 @@ message(STATUS CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
 
 message(STATUS "platform: ${CMAKE_SYSTEM_NAME}")
 
-if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+if(CMAKE_SYSTEM_NAME MATCHES "Windows")
     add_compile_definitions(_CRT_SECURE_NO_WARNINGS)
-elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
-elseif(${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
+elseif(CMAKE_SYSTEM_NAME MATCHES "Linux")
+elseif(CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
 endif()
 
 message(STATUS "compiler: ${CMAKE_CXX_COMPILER_ID}")
 
-if(${CMAKE_CXX_COMPILER_ID} MATCHES Clang)
+if(CMAKE_CXX_COMPILER_ID MATCHES Clang)
     add_compile_options("-Wall")
-elseif(${CMAKE_CXX_COMPILER_ID} MATCHES GNU)
+elseif(CMAKE_CXX_COMPILER_ID MATCHES GNU)
     # add_compile_options("-Werror")
     add_compile_options("-Wall")
 
-    if(NOT WIN32)
+    if(NOT CMAKE_SYSTEM_NAME MATCHES "Windows")
         if("${CMAKE_BUILD_TYPE}" STREQUAL "Debug" OR "${CMAKE_BUILD_TYPE}" STREQUAL "RelWithDebInfo")
             add_compile_options(-fsanitize=leak -fsanitize=address -fno-omit-frame-pointer)
             set(LINK_LIBRARIES asan)
         endif()
     endif()
 
-elseif(${CMAKE_CXX_COMPILER_ID} MATCHES Intel)
-elseif(${CMAKE_CXX_COMPILER_ID} MATCHES MSVC)
+elseif(CMAKE_CXX_COMPILER_ID MATCHES Intel)
+elseif(CMAKE_CXX_COMPILER_ID MATCHES MSVC)
     # 移除默认的警告级别，然后设置 W4
     string(REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
     string(REGEX REPLACE "/W[0-4]" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
